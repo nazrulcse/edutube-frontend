2d054d8db74acf6c42e274c863947a1de8da5a16
Nazrul <nazrulku07@gmail.com>
2018-05-05 13:08:46 +0600

    generate user profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,16 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EditComponent } from './users/profile/edit/edit.component';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './users/profile/profile.component';
 
 // Route Configuration
 export const routes: Routes = [
-{
+  {
     path: '',
     component: HomeComponent // Remember to import the Home Component
   },
-  { path: 'profile/edit', component: EditComponent }
+  { path: 'profile/edit', component: EditComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 export const Routing: ModuleWithProviders = RouterModule.forRoot(routes);
