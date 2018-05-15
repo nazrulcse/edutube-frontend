86a0fca608b94cd9f743857bc44de2293da7b3ac
Nazrul <nazrulku07@gmail.com>
2018-05-15 14:52:13 +0600

    update course content and design course settings page

--- src/app/models/course.ts
+++ src/app/models/course.ts
@@ -1,5 +1,18 @@
 export class Course {
 	id: number;
 	title: string;
+	sub_title: string;
+	description: string;
+	language: string;
+	status: string;
 	is_test: boolean;
+	tools_required: string;
+	who_can_take: string;
+	achivement: string;
+	image: string;
+	promo_video: string;
+	discount_price: number;
+	price: number;
+	welcome_message: string;
+	congratulation_message: string;
 }
--- src/app/users/instructor/dashboard/courses/edit/edit-course.component.ts
+++ src/app/users/instructor/dashboard/courses/edit/edit-course.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import { CourseService } from "../../../../../../services/course_service";
 import { Course } from "../../../../../models/course";
+import { environment } from '../../../../../../environments/environment';
 
 @Component({
   selector: 'app-edit-course',
@@ -12,22 +13,62 @@ export class EditCourseComponent implements OnInit {
 
   course: Course;
   error = '';
+  display_discount = false;
+  uploading_image = false;
+  uploading_video = false;
+  course_id = '';
+  env: any;
   constructor(private courseService: CourseService,
   	private route:ActivatedRoute) { 
   	this.course = new Course();
+    this.env = environment;
   }
 
   ngOnInit() {
-  	let course_id =  this.route.snapshot.params['id'];
-  	this.loadCourse(course_id);
+  	this.course_id =  this.route.snapshot.params['id'];
+  	this.loadCourse(this.course_id);
   }
 
-  public submitCourseGoal() {
-
+  public submitCourseUpdate() {
+    this.courseService.updateCourse(this.course).subscribe(response => {
+      if(response.success) {
+        this.notification(true, response.message);
+      }
+      else {
+        this.notification(false, response.message);
+      }
+    },
+    err => {
+      this.notification(false, 'Something wrong! Please try after sometimes');
+    })
   }
 
-  public submitCoursePrice() {
-
+  public onFileChanged($event, type) {
+    const formData = new FormData();
+    var file = $event.target.files[0];
+    formData.append('file', file, file.name);
+    formData.append('upload_type', type);
+    if(type == 'image') {
+      this.uploading_image = true;
+    }
+    else {
+      this.uploading_video = true;
+    }
+    this.courseService.uploadFile(this.course_id, formData).subscribe(response => {
+      if(type == 'image') {
+        this.course.image = response.file;
+        this.uploading_image = false;
+      }
+      else {
+        this.course.promo_video = response.file;
+        this.uploading_video = false;
+      }
+    },
+    err => {
+      this.error = "Unable to upload course " + type;
+      this.uploading_image = false;
+      this.uploading_video = false;
+    });
   }
 
   public submitCourseDetails() {
@@ -40,6 +81,14 @@ export class EditCourseComponent implements OnInit {
 
   public submitCourseCaptions() {
     
+  }
+
+  public addDiscountPrice() {
+    this.display_discount = true;
+  }
+
+  public showDiscount() {
+    return (this.course.discount_price > 0 || this.display_discount);
   }
 
   public loadCourse(id) {
