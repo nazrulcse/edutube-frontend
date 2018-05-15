b53c2966d60feb23f979d6d7cf16683ee133199e
Nazrul <nazrulku07@gmail.com>
2018-05-15 19:49:09 +0600

    update teacher profile infor

--- src/app/users/profile/teacher/teacher.component.ts
+++ src/app/users/profile/teacher/teacher.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Education } from '../../../models/education';
+import { Experience } from '../../../models/experience';
+import { UserService } from '../../../../services/user_service';
+import { EventService } from '../../../../services/event_service';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-teacher-profile',
@@ -8,9 +13,35 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TeacherProfileComponent implements OnInit {
 
   @Input() user;
-  constructor() { }
+  educations: Array<Education>;
+  experiences: Array<Experience>;
+  env: any;
+  error = '';
+  constructor(private userService: UserService) { 
+    this.env = environment;
+  }
 
   ngOnInit() {
+  	this.loadEducation();
+  	this.loadExperiences();
+  }
+
+  public loadEducation() {
+    this.userService.getEducations().subscribe(response => {
+      this.educations = response.educations;
+    },
+    err => {
+      this.error = "Unable to load some profile data!";
+    });
+  }
+
+  public loadExperiences() {
+    this.userService.getExperiences().subscribe(response => {
+      this.experiences = response.experiences;
+    },
+    err => {
+      this.error = "Unable to load some profile data!";
+    });
   }
 
 }
